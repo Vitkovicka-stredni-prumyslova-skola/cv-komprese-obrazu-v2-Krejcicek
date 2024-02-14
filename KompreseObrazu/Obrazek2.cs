88a726269757430c8f76335d4c78d48dff806786
Krejcicek <94050047+Krejcicek@users.noreply.github.com>
2024-02-14 09:37:39 +0000

    cv-komprese-pracesobrazkem(n)

---
+++ KompreseObrazu/Obrazek2.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography.X509Certificates;
+public class Obrazek2
+{
+   private int [] [] Pole;
+    public void VyhledatPix()
+    {
+        
+    }
+    public void SpocictatPix()
+    {
+     
+    }
+}
+        
+    
